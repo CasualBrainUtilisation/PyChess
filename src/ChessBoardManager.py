6ad2154afb080a0b5fe17649476739de324ea9c3
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-12 15:13:46 +0100

    added FieldHandler class

--- src/ChessBoardManager.py
+++ src/ChessBoardManager.py
@@ -10,4 +10,26 @@ class ChessBoard:
         #consts
         self.CELLSIZE = CELLSIZE;
         
+        #board Parts
+        self.fieldHandler: FieldHandler = FieldHandler(); #class that handles all the fields on the chessBoard
+        
+        pass
+    
+    def createBoard(self): #method that creates a canvas on the self.window, that'll contain the chessBoard
+        
+        #cycly through the whole board and add the field backgrounds
+        
+        pass;
+    
+    
+
+class FieldHandler: #class that handles all the fields of a chess board
+    #consts
+    COLORS: tuple = ("white", "black") #colors used for the fields on the board
+    
+    def __init__(self) -> None:
+        pass
+    
+    def createBoardFields(self, chessBoard: ChessBoard): #method that'll create the fields for a chessBoard on given chessBoard
+        #TODO fieldCreation logic
         pass
