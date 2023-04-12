1c35b75a34fe32486f7e252121d0c3eb93e3b829
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-12 15:05:39 +0100

    structured project

--- main.py
+++
@@ -1,3 +0,0 @@
-# this is the main .py file for the chessGame, that'll control all the other classes/modules
-
-
---
+++ src/main.py
@@ -0,0 +1,20 @@
+# this is the main .py file for the chessGame, that'll control all the other classes/modules
+
+#window setup
+from tkinter import *; #import tkinter
+
+window = Tk(); # create the main window
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop();
