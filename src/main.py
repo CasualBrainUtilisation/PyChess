eb392eb67f9483cce39dc64c03874dbc59e0f337
CasualBrainUtilisation <120629434+CasualBrainUtilisation@users.noreply.github.com>
2023-04-16 17:15:31 +0000

    updated main.py comments

--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 # this is the main .py file for the chessGame, that'll control all the other classes/modules
 
 #window setup
-from tkinter import *
-#import tkinter
-
-window: Tk = Tk()
-# create the main window
+from tkinter import *  #import tkinter
+window: Tk = Tk()  # create the main window
 
 #Chess game setup
-from ChessBoardManager import *
-
-chessBoardManager = ChessBoard(window, 60)
+from ChessBoardManager import *  #first we import the ChessBoardMangaer.py, which is used to manage the whole chess game
+chessBoardManager = ChessBoard(window, 60)  #create a new ChessBoard class, which itself sets up everything else to start a chess game
 
 window.mainloop()
